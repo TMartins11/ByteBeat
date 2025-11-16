f34a132c4acd4501cf7b5f3ca9c4b024fb3c3135
Thiago Martins <thiago.martins1112@gmail.com>
2025-11-15 23:35:24 -0300

    API demo de uma livraria, feita para exercitar bases do protoclo HTML e SpringBoot.

---
+++ src/test/java/com/mrtns/spring/CrudRestJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrtns.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestJpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
